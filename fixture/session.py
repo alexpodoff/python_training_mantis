433f653ad22b55fd7d01422d1ed9dba0050ab220
Alexandr Struchkov <alexpodoff@gmail.com>
2016-12-24 12:11:41 +0300

    login as admin test added

---
+++ fixture/session.py
@@ -0,0 +1,45 @@
+
+
+class SessionHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("Logout")) > 0
+
+    def is_logged_in_as(self, username):
+        return self.get_loged_user() == username
+
+    def get_loged_user(self):
+        wd = self.app.wd
+       # return wd.find_element_by_css_selector("td.logged-in-user span").text
+        return wd.find_element_by_xpath("//div/div[2]/div[1]/span[@id='logged-in-user']").text
+
+    def login(self, username, passwd):
+        wd = self.app.wd
+        self.app.open_home_page()
+        wd.find_element_by_name("username").click()
+        wd.find_element_by_name("username").clear()
+        wd.find_element_by_name("username").send_keys(username)
+        wd.find_element_by_name("password").click()
+        wd.find_element_by_name("password").clear()
+        wd.find_element_by_name("password").send_keys(passwd)
+        wd.find_element_by_css_selector('input[type="submit"]').click()
+
+    def enshure_login(self, username, passwd):
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, passwd)
+
+    def logout(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("Logout").click()
+
+    def enshure_logout(self):
+        if self.is_logged_in():
+            self.logout()
